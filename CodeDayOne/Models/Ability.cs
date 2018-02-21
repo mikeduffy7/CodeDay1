b2899bcbedeaa2fae7518f0690ec18621fdc3de8
R1zzo23 <adamjrizzo@yahoo.com>
2018-02-21 12:34:16 -0500

    Edited controller to create Sona

--- CodeDayOne/Models/Ability.cs
+++ CodeDayOne/Models/Ability.cs
@@ -5,15 +5,21 @@ namespace CodeDayOne.Models
     {
         public string Name { get; set; }
         public int ManaCost { get; set; }
-        public DamageType DamageType { get; set; }
+        public Damage_Type _DamageType { get; set; }
         public int CoolDown { get; set; }
 
-        public Ability(string name, int manaCost, DamageType damageType, int coolDown)
+        public Ability(string name, int manaCost, Damage_Type damageType, int coolDown)
         {
             this.Name = name;
             this.ManaCost = manaCost;
-            this.DamageType = damageType;
+            this._DamageType = damageType;
             this.CoolDown = coolDown;
+        }
+
+        public enum Damage_Type
+        {
+            physical,
+            magic
         }
     }
 }
