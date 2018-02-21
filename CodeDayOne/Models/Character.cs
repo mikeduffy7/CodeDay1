6df9ced3e4acf32c88f0a432db86956864045bd3
R1zzo23 <adamjrizzo@yahoo.com>
2018-02-21 11:36:16 -0500

    Updated Character class

--- CodeDayOne/Models/Character.cs
+++ CodeDayOne/Models/Character.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CodeDayOne.Models
 {
     public class Character
     {
-        public Character()
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackSpeed { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Armor { get; set; }
+        public int MagicResist { get; set; }
+        public List<Ability> AbilityList { get; set; }
+
+        public Character(string name, int health, int attackSpeed, int attack, 
+                         int defense, int armor, int magicResist, List<Ability> abilityList)
         {
+            Name = name;
+            Health = health;
+            AttackSpeed = attackSpeed;
+            Attack = attack;
+            Defense = defense;
+            Armor = armor;
+            MagicResist = magicResist;
+            AbilityList = abilityList;
         }
     }
 }
