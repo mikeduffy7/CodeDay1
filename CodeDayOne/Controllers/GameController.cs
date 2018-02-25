1a8d9d0e55fe4a45b2fc94aa638c6c7ccde47f0f
R1zzo23 <adamjrizzo@yahoo.com>
2018-02-25 16:53:42 -0500

    Created Game view and controller; selecting champ works!

---
+++ CodeDayOne/Controllers/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CodeDayOne.Models;
+
+namespace CodeDayOne.Controllers
+{
+    public class GameController : Controller
+    {
+        public Game game = new Game();
+
+
+        // GET: Game
+        public IActionResult PartySelect()
+        {
+            game.ChampionList = game.LoadChampions();
+            
+            return View(game);
+        }
+
+        public ActionResult ChangeChampion(int champIndex)
+        {
+            game.ChampionList = game.LoadChampions();
+            game.Player1 = game.ChampionList[champIndex];
+            Console.Write("Party Member #1: " + game.Player1.Name);
+            return View(game);
+        }
+    }
+}
