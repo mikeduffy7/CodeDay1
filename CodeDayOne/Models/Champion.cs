4aacba75f97bc776ef6c67f65f7d3747b02aca33
Adam Rizzo <adamjrizzo@yahoo.com>
2018-02-28 07:28:49 -0500

    Character & Champion creation now gives max health, max mana and XP.

--- CodeDayOne/Models/Champion.cs
+++ CodeDayOne/Models/Champion.cs
@@ -5,14 +5,18 @@ namespace CodeDayOne.Models
 {
     public class Champion : Character
     {
-        public int Mana { get; set; }
+        public int MaxMana { get; set; }
+        public int CurrentMana { get; set; }
+        public int XP { get; set; }
         public string ImageSource { get; set; }
 
-        public Champion(string name, int health, int attackSpeed, int attack,
-                         int defense, int armor, int magicResist, List<Ability> abilityList, int mana) 
-            : base(name, health, attackSpeed, attack, defense, armor, magicResist, abilityList)
+        public Champion(string name, int maxHealth, int attackSpeed, int attack,
+                         int defense, int armor, int magicResist, List<Ability> abilityList, int maxMana) 
+            : base(name, maxHealth, attackSpeed, attack, defense, armor, magicResist, abilityList)
         {
-            this.Mana = mana;
+            this.MaxMana = maxMana;
+            this.CurrentMana = maxMana;
+            this.XP = 0;
             //ImageSource creates the URL to grab a champion's image from the LoL website
             this.ImageSource = "https://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/" + name + ".png";
         }
--- CodeDayOne/Models/Character.cs
+++ CodeDayOne/Models/Character.cs
@@ -6,7 +6,8 @@ namespace CodeDayOne.Models
     public class Character
     {
         public string Name { get; set; }
-        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int CurrentHealth { get; set; }
         public int AttackSpeed { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
@@ -14,11 +15,12 @@ namespace CodeDayOne.Models
         public int MagicResist { get; set; }
         public List<Ability> AbilityList { get; set; }
 
-        public Character(string name, int health, int attackSpeed, int attack, 
+        public Character(string name, int maxHealth, int attackSpeed, int attack, 
                          int defense, int armor, int magicResist, List<Ability> abilityList)
         {
             this.Name = name;
-            this.Health = health;
+            this.MaxHealth = maxHealth;
+            this.CurrentHealth = maxHealth;
             this.AttackSpeed = attackSpeed;
             this.Attack = attack;
             this.Defense = defense;
