6824796538de6f3a4127c509c72e9c0f07698813
Adam Rizzo <adamjrizzo@yahoo.com>
2018-03-27 07:34:20 -0400

    Frustration is setting in.

--- CodeDayOne/wwwroot/js/app.js
+++ CodeDayOne/wwwroot/js/app.js
@@ -1,27 +1,22 @@
-﻿$(document).ready(function () {
-    
-});
+﻿function attack() {
+    var div = document.getElementById("battleDialogue");
+    div.innerHTML += "Champion used basic attack against minion.";
+}
 
 function highlightMinion(x) {
     var minions = document.querySelectorAll(".battleMinion");
     console.log = minions.length;
-    if ($(x).hasClass('targeted')) {
-        $(x).removeClass('targeted');
-        $(x).css('background-color', '#ffffff');
+    if ($(x).hasClass("targeted")) {
+        $(x).removeClass("targeted");
+        $(x).css("background-color", "#ffffff");
     }
     else {
         for (var i = 0; i < minions.length; i++) {
-            minions[i].classList.remove('targeted');
+            minions[i].classList.remove("targeted");
             minions[i].style.backgroundColor = "white";
         }
-        $(x).css('background-color', '#fff375');
-        $(x).addClass('targeted');
+        $(x).css("background-color", "#fff375");
+        $(x).addClass("targeted");
     }
-    var div = document.getElementById('battleDialogue');
-    div.innerHTML += "Minion has been targeted!";
-}
-
-function attack() {
-    var div = document.getElementById('battleDialogue');
-    div.innerHTML += "Champion used basic attack against minion.";
+    $("#battleDialogue").text("Targeted a minion!");
 }
